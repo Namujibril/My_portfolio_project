777b23913d521fc6537dea7c2bc9bef1ca1ab62e
Namujibril <Namujibril@gmail.com>
2024-06-24 09:21:21 +0000

    Adding components to Main.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Footer from "./components/Footer"
+import Home from "./components/Home"
+import Project from "./components/Project"
+import Skills from "./components/Skills"
+
+export default function App() {
+  return (
+    import React from 'react'
+    
+    const App = () => {
+      return (
+        <main>
+        <Footer/>
+        <Project/>
+        <Skills/>
+        <Home/>
+        </main>
+      )
+    }
+    
+    export default App
+    
+  )
+}
---
+++ src/components/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default Skills
